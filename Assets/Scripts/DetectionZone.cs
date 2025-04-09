a9f53b580670ac7b133b4f8879f65453dc62a876
s19260 <80157999+s19260@users.noreply.github.com>
2025-04-09 16:08:34 +0200

    Shadow script collisions - error Cannot resolve symbol 'animator'

--- Assets/Scripts/DetectionZone.cs
+++ Assets/Scripts/DetectionZone.cs
@@ -1,27 +1,33 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectionZone : MonoBehaviour
 {
-    public List<Collider2D> collidersInZone = new List<Collider2D>();
-    Collider2D detectionCol;
+    public List<Collider2D> detectedColliders = new List<Collider2D>();
+    Collider2D col; 
     public int colliderCount;
+
+
+    private void Awake()
+    {
+        col = GetComponent<Collider2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        detectionCol = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        collidersInZone.Add(collider);
-        colliderCount = collidersInZone.Count;
+        detectedColliders.Add(collider);
+        //colliderCount = detectedColliders.Count;
     }
 
     private void OnTriggerExit2D(Collider2D collider)
     {
-        collidersInZone.Remove(collider);
-        colliderCount = collidersInZone.Count;
+        detectedColliders.Remove(collider);
+      // colliderCount = detectedColliders.Count;
     }
 }
--- Assets/Scripts/Shadow.cs
+++ Assets/Scripts/Shadow.cs
@@ -1,29 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class Shadow : MonoBehaviour
 {
+    public enum WalkableDirection
+    {
+        Right,
+        Left
+    }
 
     public float walkSpeed = 5f;
+    public DetectionZone attackZone;
 
-    Rigidbody2D  rb;
-    TouchingDirections touchingDirections;
-        public DetectionZone clifEdgeZone;
+    [SerializeField] public WalkableDirection _walkDirection;
 
-
-    public enum WalkableDirection { Right, Left};
-    [SerializeField]
-    public WalkableDirection _walkDirection;
     public Vector2 walkDirectionVector = Vector2.right;
-    Vector2 walkDirectionAsVector2;
+
+    public bool _hasTarget;
+
+    private Rigidbody2D rb;
+    private TouchingDirections touchingDirections;
+    private Vector2 walkDirectionAsVector2;
 
     public WalkableDirection WalkDirection
     {
-        get
-        {
-            return _walkDirection;
-        }
+        get => _walkDirection;
         set
         {
             // Make changes only if new
@@ -37,7 +38,6 @@ public class Shadow : MonoBehaviour
             }
             else if (value == WalkableDirection.Right)
             {
-
                 // Facing right so positive scale
                 transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
                 walkDirectionAsVector2 = Vector2.right;
@@ -46,31 +46,43 @@ public class Shadow : MonoBehaviour
             touchingDirections.wallCheckDirection = walkDirectionAsVector2;
         }
     }
-    
+
+    public bool HasTarget
+    {
+        get => _hasTarget;
+        private set
+        {
+            _hasTarget = value;
+           animator.SetBool(AnimationStrings.hasTarget, value);
+        }
+    }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         touchingDirections = GetComponent<TouchingDirections>();
     }
 
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        HasTarget = attackZone.detectedColliders.Count > 0;
+    }
+
     private void FixedUpdate()
     {
         touchingDirections.wallCheckDirection = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
 
-        if (touchingDirections.IsGrounded && touchingDirections.IsOnWall)
-        {
-            FlipDirection();
-        }
+        if (touchingDirections.IsGrounded && touchingDirections.IsOnWall) FlipDirection();
 
-        float tempWalkSpeed = walkSpeed;
-        if (WalkDirection == WalkableDirection.Left)
-        {
-            tempWalkSpeed = -tempWalkSpeed;
-        }
+        var tempWalkSpeed = walkSpeed;
+        if (WalkDirection == WalkableDirection.Left) tempWalkSpeed = -tempWalkSpeed;
 
         rb.linearVelocity = new Vector2(tempWalkSpeed, rb.linearVelocity.y);
     }
-
 
 
     private void FlipDirection()
@@ -89,15 +101,5 @@ public class Shadow : MonoBehaviour
                 Debug.Log(name + "'s WalkDirection is not set to Left or Right");
                 break;
         }
-    }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
