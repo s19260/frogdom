bb039e7160f371171e90bf960a3506ffab3d7909
s19260 <80157999+s19260@users.noreply.github.com>
2025-04-20 11:13:52 +0200

    Changes to script to adapt knockback effect, 3:55

--- Assets/Scripts/AnimationStrings.cs
+++ Assets/Scripts/AnimationStrings.cs
@@ -16,5 +16,6 @@ internal class AnimationStrings
     internal static string hasTarget = "hasTarget";
     internal static string isAlive = "isAlive";
     internal static string canMove = "canMove";
+    internal static string isHit = "isHit";
 
 }
--- Assets/Scripts/SetBoolBehavior.cs
+++ Assets/Scripts/SetBoolBehavior.cs
@@ -8,6 +8,7 @@ public class SetBoolBehavior : StateMachineBehaviour
 
     public bool setOnStateEnterAndExit = false;
     public bool trueWhileInState = true;
+    
 
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
