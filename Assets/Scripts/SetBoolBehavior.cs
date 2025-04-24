c528794e850cfb2d799931408126887fb76f916d
s19260 <80157999+s19260@users.noreply.github.com>
2025-04-24 13:01:26 +0200

    SetBoolBehavior script update

--- Assets/Scripts/SetBoolBehavior.cs
+++ Assets/Scripts/SetBoolBehavior.cs
@@ -6,24 +6,38 @@ public class SetBoolBehavior : StateMachineBehaviour
     public bool setOnStateMachineEnterAndExit;
     public bool trueWhileInMachine = true;
 
-    public bool setOnStateEnterAndExit = false;
+    public bool setOnStateEnter = false;
+    public bool setOnStateExit  = false;
     public bool trueWhileInState = true;
+    public bool updateOnState = false;
+    public bool updateOnStateMachine = false;
     
 
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (setOnStateEnterAndExit)
+        if (setOnStateEnter)
             animator.SetBool(booleanName, trueWhileInState);
     }
 
     // OnStateExit is called before OnStateExit is called on any state inside this state machine
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (setOnStateEnterAndExit)
+        if (setOnStateExit)
             animator.SetBool(booleanName, !trueWhileInState);
     }
 
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        if (updateOnState)
+            animator.SetBool(booleanName, trueWhileInState);
+    }
+
+    public void OnStateMachineUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(updateOnStateMachine)
+            animator.SetBool(booleanName, trueWhileInState);
+    }
     // OnStateMachineEnter is called when entering a state machine via its Entry Node
     override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
     {
