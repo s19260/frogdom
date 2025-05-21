4bac5f08d390483792526fff7762c0c54df15645
s19260 <80157999+s19260@users.noreply.github.com>
2025-05-21 18:00:16 +0200

    Jump power up got after getting 10 trash, bar resets to 1

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -3,10 +3,10 @@ using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
-    
     public Slider progressBar;          
     public int totalCollectibles = 10;  
     private int collected = 0;          
+    public GameObject jumpPowerUpPrefab; // Add this field for the power-up prefab
 
     void Start()
     {
@@ -16,9 +16,19 @@ public class ProgressBar : MonoBehaviour
         progressBar.value = 0;
     }
 
-    public void Collect()
+    public void Collect(Transform playerTransform) // Add player transform parameter
     {
         collected++;
         progressBar.value = collected;
+
+        if (collected >= totalCollectibles)
+        {
+            collected = 0;
+            progressBar.value = collected;
+            
+            // Spawn power-up near player
+            Vector3 spawnPosition = playerTransform.position + new Vector3(1, 0, 0);
+            Instantiate(jumpPowerUpPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
--- Assets/Scripts/TrashPickup.cs
+++ Assets/Scripts/TrashPickup.cs
@@ -1,19 +1,15 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class TrashPickup : MonoBehaviour
 {
-    
-    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Trash Pickup");
-            FindObjectOfType<ProgressBar>().Collect();
+            // Pass the player's transform to the Collect method
+            FindObjectOfType<ProgressBar>().Collect(other.transform);
             Destroy(gameObject);
-            
         }
-
     }
 }
