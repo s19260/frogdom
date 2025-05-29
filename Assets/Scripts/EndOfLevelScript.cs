90ea75a9cd0b50633abb2ee61994c3ef011e3873
s19260 <80157999+s19260@users.noreply.github.com>
2025-05-29 15:34:37 +0200

    Script changes

---
+++ Assets/Scripts/EndOfLevelScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfLevelScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Level ended");
+         //   SceneManager.LoadScene("MainMenuScene");
+        }
+    }
+
+}
+
