7628356772031dac05a4059dadffe9c2d9564dc5
s19260 <80157999+s19260@users.noreply.github.com>
2025-06-06 14:22:08 +0200

    Platform bounce correction

--- Assets/Scripts/PlatformBounce.cs
+++ Assets/Scripts/PlatformBounce.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class PlatformBounce : MonoBehaviour
 {
     private Rigidbody2D playerRigidBody;
-    Animator animator;
+    private Animator animator;
+
     private void Start()
     {
         playerRigidBody = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); // Initialize animator component
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -14,7 +16,7 @@ public class PlatformBounce : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("collision enter with player");
-       //     animator.SetTrigger(AnimationStrings.isBounce);
+            animator.SetTrigger("isBounce"); // Trigger bounce animation
             playerRigidBody.AddForce(new Vector2(0, 20), ForceMode2D.Impulse);
         }
     }
