b1a36634545f317d1c05e646e8446f728d6a928b
s19260 <80157999+s19260@users.noreply.github.com>
2025-06-17 10:00:03 +0200

    Prefabs

--- Assets/Scripts/TrashPickup.cs
+++ Assets/Scripts/TrashPickup.cs
@@ -2,14 +2,33 @@ using UnityEngine;
 
 public class TrashPickup : MonoBehaviour
 {
+    public AudioClip pickupSound; 
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
 //            Debug.Log("Trash Pickup");
             // Pass the player's transform to the Collect method
+            PlayPickupSound();
             FindObjectOfType<ProgressBar>().Collect(other.transform);
-            Destroy(gameObject);
+            GetComponent<SpriteRenderer>().enabled = false;
+             Destroy(gameObject, pickupSound.length);
         }
     }
+    
+    private void PlayPickupSound()
+    {
+        if (pickupSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(pickupSound);
+        }
+    }
+
 }
